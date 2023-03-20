13ae1980883dcd959de0f92e8e65a8ecccf26c22
chandaja <124127779+chandaja@users.noreply.github.com>
2023-03-20 14:25:53 +0530

    Update demo.java

--- demo.java
+++ demo.java
@@ -1,1 +1,16 @@
+import java.util.Arrays;
 
+public class Main {
+public static void main(String[] args) {
+        int j=2;
+        int[] c={1,2,3,4,5,6,7,8,9,10};
+        for(int l=0;l<j;l++){
+        int t=c[0];
+        for (int a=0;a<c.length-1;a++){
+        c[a]=c[a+1];
+        }
+        c[c.length-1]=t;
+        }
+        System.out.println("left shift: "+ Arrays.toString(c));
+        }
+        }
